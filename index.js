d0909910bdf9f7e544cf545b0655ef74e20ef93f
Daniel Ward <danielaj_28@hotmail.co.uk>
2021-02-01 01:11:56 +0000

    init

---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+var path = require("path");
+
+let data = `<?xml version="1.0" standalone="no"?>
+<mlt LC_NUMERIC="C" version="6.23.0" title="Shotcut version 20.10.31" producer="main_bin">
+  <profile description="PAL 4:3 DV or DVD" width="1920" height="1080" progressive="1" sample_aspect_num="1" sample_aspect_den="1" display_aspect_num="16" display_aspect_den="9" frame_rate_num="30" frame_rate_den="1" colorspace="709"/>
+  `;
+
+let videoFiles = [];
+
+fs.readdirSync("./bin/").forEach((file) => {
+  videoFiles.push((path.resolve("./bin/") + "/" + file).replace(/\\/g, "/"));
+});
+
+let count = 0;
+
+videoFiles.forEach((path) => {
+  data += `<producer id="producer${count}">
+    <property name="resource">${path}</property>
+  </producer>`;
+  count++;
+});
+
+data += `<playlist id="main_bin" title="Shotcut version 20.10.31">
+<property name="shotcut:projectAudioChannels">2</property>
+<property name="shotcut:projectFolder">0</property>
+<property name="xml_retain">1</property>
+`;
+
+let producers = "";
+
+for (let index = 0; index < videoFiles.length; index++) {
+  producers += `<entry producer="producer${index}"/>`;
+}
+
+data += producers;
+
+data += `</playlist>
+<producer id="black">
+  <property name="eof">pause</property>
+  <property name="resource">0</property>
+  <property name="aspect_ratio">1</property>
+  <property name="mlt_service">color</property>
+  <property name="mlt_image_format">rgb24a</property>
+  <property name="set.test_audio">0</property>
+</producer>
+<playlist id="background">
+  <entry producer="black"/>
+</playlist>
+<playlist id="playlist0">
+  <property name="shotcut:video">1</property>
+  <property name="shotcut:name">V1</property>
+  `;
+
+data += producers;
+
+data += `
+  </playlist>
+  <tractor id="tractor0" title="Shotcut version 20.10.31" global_feed="1">
+    <property name="shotcut">1</property>
+    <property name="shotcut:projectAudioChannels">2</property>
+    <property name="shotcut:projectFolder">0</property>
+    <track producer="background"/>
+    <track producer="playlist0"/>
+  </tractor>
+</mlt>`;
+
+try {
+  fs.writeFileSync("out.mlt", data);
+  console.log("Success in creating project file!");
+} catch (err) {
+  console.error(`Error: ${err}`);
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Video Mash
+
+A system for generating a Shotcut project file containing all sample videos in a folder ready to but cut down.
+
+## Todo
+
+- [ ] List files in directory
+- [ ] Understand how duration plays into this
+- [ ] Create project file
+
+## Requirements
+
+1. [Shotcut Video Editor](https://shotcut.org/download/)
+
+## How to Run
+
+1. Put video files into bin dir
+1. Run program
+1. Open the generated Shotcut project file Shotcut editor 
